8c424db011913e91777fa4fd8b66d34257d127e3
Victor Pavon <vpavonru@alumnos.unex.es>
2013-11-04 20:21:12 +0100

    Definido el bean para una tarea

---
+++ src/main/java/eitie/toDoList/bean/Tarea.java
@@ -0,0 +1,29 @@
+package eitie.toDoList.bean;
+
+import java.util.Date;
+
+public class Tarea {
+	int 	id;
+	String 	titulo;
+	String 	descripcion;
+		
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+}
+
